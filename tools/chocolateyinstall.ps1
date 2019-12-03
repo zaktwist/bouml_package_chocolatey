245fa63752ce7f21f0c0d304b2973c15187c15fb
riccardo <riccardo.rizzo@archimede-srl.com>
2019-12-03 18:48:12 +0100

    Update bouml.nuspec and chocolateyinstall.ps1 init package bouml https://www.bouml.fr/files/Bouml_7.9_setup.exe

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,40 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.bouml.fr/files/Bouml_7.9_setup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'Bouml*'
+
+  checksum      = '4b0ff3627bc38747e439c6f73a1171afcd9b550b5e772fe2c9121412a48fe04d'
+  checksumType  = 'sha256'
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
