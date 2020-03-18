7bc6beb673b4c084317dc6e7e99fc0d4fc1f9636
zaktwist <zaktwist@gmail.com>
2020-03-18 11:12:07 +0100

    Update bouml.nuspec and chocolateyinstall.ps1 upgrade version: 7.9 -> 7.9.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.bouml.fr/files/Bouml_7.9_setup.exe'
+$url        = 'https://www.bouml.fr/files/Bouml_7.9.1_setup.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,8 +10,8 @@ $packageArgs = @{
   url           = $url
 
   softwareName  = 'Bouml*'
-
-  checksum      = '4b0ff3627bc38747e439c6f73a1171afcd9b550b5e772fe2c9121412a48fe04d'
+                  
+  checksum      = 'aa4b9cda5832eaa341aedfb6f5f7e56482aa429b955b46782c294cdbe9328c43'
   checksumType  = 'sha256'
 
   validExitCodes= @(0, 3010, 1641)
